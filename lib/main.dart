6834715b28378bc2740011cfce8921fd54d62702
Kristina Hrynko <grnkkristina@gmail.com>
2025-08-19 07:41:28 +0300

    2 challenge

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,23 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         title: Text("Ask Me Anything"),
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
